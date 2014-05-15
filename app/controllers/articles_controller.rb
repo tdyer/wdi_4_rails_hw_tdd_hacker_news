c2322e4f994d92892a66720a780c7e5fdd6c1d50
Tom Dyer <tdyer@ourstage.com>
2014-05-14 21:06:22 -0400

    Added Article create, show and index views

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit]
+
+  # GET /articles
+  def index
+    @articles = Article.all.order(submitted_at: :desc)
+  end
+
+  # GET /articles/1
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+    
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.' 
+    else
+      render :new
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def article_params
+    params.require(:article).permit(:title, :url)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.delete_all
+puts "Creating Articles"
+
+Article.create!(title: 'Next IPhone rumors', url: 'http://www.theverge.com/2014/5/14/5718356/apple-iphone-6-rumors-90sotv')
+
+Article.create!(title: 'Lawsuit alleges unauthorized publication of personal genetics data', url: 'http://arstechnica.com/tech-policy/2014/05/lawsuit-alleges-unauthorized-publication-of-personal-genetics-data/')
+
+Article.create!(title: 'What Is Tor and Should I Use It?', url: 'http://lifehacker.com/what-is-tor-and-should-i-use-it-1527891029')
+
+Article.create!(title: 'Pickle Frenzy', url: 'http://www.moddb.com/games/pickle-frenzy')
+
