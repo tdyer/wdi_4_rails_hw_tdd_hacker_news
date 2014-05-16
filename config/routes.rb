fd6a03ae5f52d93892ee544033f30d0756d524fa
Tom Dyer <tdyer@ourstage.com>
2014-05-16 04:03:40 -0400

    Added a users controller, should me managed by an admin in the future.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
+
+  resources :users, only: [:index, :show]
   
   resources :articles, only: [:index, :show, :new, :create] do
     resources :comments, only: [:new, :create]
