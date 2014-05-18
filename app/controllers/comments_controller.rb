3a62e4367ad6e95c444309f9a469e3cd01a68968
Tom Dyer <tdyer@ourstage.com>
2014-05-18 00:10:00 -0400

    Added a view to show a comment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
+  before_action :set_comment, only: [:show]  
   before_action :set_article, only: [:new,:create]
 
+  def show
+  end
+  
   def new
     @comment = Comment.new
   end
@@ -23,7 +27,11 @@ class CommentsController < ApplicationController
   def set_article
     @article = current_user.articles.find(params[:article_id])
   end
-  
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def comment_params
     params[:comment][:user_id] = current_user.id
     params.require(:comment).permit(:content, :user_id, :article_id)
