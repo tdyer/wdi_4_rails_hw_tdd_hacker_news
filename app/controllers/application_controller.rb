8223e92b9a5f85a813fbd792d685de9eddaf9e5d
Tom Dyer <tdyer@ourstage.com>
2014-05-17 23:30:49 -0400

    Redirect to last path or user view after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    # resource in a User object
+    # Go back to failed location, stored in session. Or go to the user's show action
+    stored_location_for(resource) || user_url(resource) || signed_in_root_path(resource)
+
+  end
 end
